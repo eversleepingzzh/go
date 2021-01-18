0f2d742bacf1cb29bd27e926de118c6037b41913
zzh <951202500@qq.com>
2021-01-18 23:53:44 +0800

    add test

--- test.js
+++ test.js
@@ -5,17 +5,12 @@ test1.board =  [[0, 0, 0, 0, 0, 0, 0, 0],
  [0, 1, 1, 1, 1, 1, 0, 0],
  [0, 0, 1, 1, 0, 0, 2, 0],
  [0, 0, 0, 0, 0, 0, 0, 0],
- [0, 0, 0, 0, 0, 1, 0, 1],
- [0, 2, 0, 0, 0, 0, 0, 2],
- [0, 2, 0, 0, 0, 1, 2, 2],
+ [0, 0, 2, 0, 0, 1, 0, 1],
+ [0, 2, 1, 2, 0, 0, 0, 2],
+ [0, 2, 1, 2, 0, 1, 2, 2],
  [0, 0, 0, 0, 1, 2, 2, 2]]
 
-// console.table(test1.board)
-// test1.putChess(4, 7, 1)
-// test1.putChess(7, 4, 1)
-// test1.putChess(6, 5, 1)
-// test1.putChess(5, 6, 1)
-let res = test1.haveair(6,7, test1.board)
-test1.putChess(5, 6, 1)
+test1.putChess(7, 2, 2)
+test1.putChess(6, 2, 1)
+test1.putChess(5, 2, 1)
 console.table(test1.board)
-console.log(res)
